881c4cfbf3b50eb2fcb5f60a5f79eb357243f61f
heroTM737 <xuantien.nguyen93@gmail.com>
2017-03-23 12:16:04 +0700

    update localize name

--- controller/api/api.js
+++ controller/api/api.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
+var _ = require('lodash');
 
 var base_url = "http://128.88.242.23:7000";
 
 module.exports = function (app) {
     var heroes_json = fs.readFileSync(__dirname + "/../../data/heroes.json", 'utf8');
     var heroes = JSON.parse(heroes_json);
+
     var data = heroes.map(function (hero) {
         return {
             name: hero.name,
+            localized_name: hero.localized_name,
             image: base_url + hero.image
         }
     });
@@ -18,14 +21,19 @@ module.exports = function (app) {
 
     app.get("/api/counter", function (req, res) {
         var name = req.query.name;
+        var target_index = _.findIndex(data, function (o) { return o.name == name; });
 
-        var r1 = Math.round(Math.random() * 113);
-        var r2 = Math.round(Math.random() * 113);
-        var r3 = Math.round(Math.random() * 113);
-        var counter_list = [heroes[r1].name, heroes[r2].name, heroes[r3].name];
+        var counter_list = [];
+        for (var i = 0; i < 3; i++) {
+            var random = Math.round(Math.random() * 113);
+            counter_list.push(data[random]);
+        }
 
-        // var counter_list = ["skywrath_mage", "crystal_maiden", "juggernaut", "nevermore", "zuus", "lina", "phantom_assassin", "techies", "terrorblade", "monkey_king"];
+        var result = {
+            target: data[target_index],
+            counter_list: counter_list
+        }
 
-        res.end(JSON.stringify(counter_list));
+        res.end(JSON.stringify(result));
     })
 }
